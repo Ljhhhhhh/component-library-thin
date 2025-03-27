14e380ef758047e2de1b14a2d480a1495af62b80
GuanMo <13216698987@163.com>
2025-03-27 13:26:06 +0800

    feat: 调整打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     }),
   ],
   build: {
-    outDir: 'js',
+    outDir: 'dist',
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'WebComponents',
